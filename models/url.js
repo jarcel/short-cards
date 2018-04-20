72149b06c6cfd5ee734ac36482a1af4146cf16ed
Jeff Arcel <jeff@jeffarcel.com>
2018-04-20 14:19:57 -0400

    adds schema for mongodb

--- models/url.js
+++ models/url.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-// create a schema
 const urlSchema = new Schema({
   _id: { type: Number, index: true },
   card_url: String,
@@ -10,9 +9,6 @@ const urlSchema = new Schema({
   created_at: Date,
 });
 
-// the schema is useless so far
-// we need to create a model using it
 const Url = mongoose.model('Url', urlSchema);
 
-// make this available to our users in our Node applications
 module.exports = Url;
